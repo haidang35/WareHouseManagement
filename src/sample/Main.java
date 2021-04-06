071ea77e977a089c6ca855b880a1f8e1b69b31b5
Linh Nguyen <libh1004@gmail.com>
2021-04-06 11:34:57 +0700

    linh update

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,8 +10,6 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        
-        
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
